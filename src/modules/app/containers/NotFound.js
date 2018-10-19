1a91d7457a141db341941f5130f038b77edc49fb
kevin <357620917@qq.com>
2018-10-19 17:05:35 +0800

    not found

--- src/modules/app/containers/NotFound.js
+++ src/modules/app/containers/NotFound.js
@@ -1,11 +1,16 @@
 /** @flow */
 import React, {Component} from "react"
+import './styles/NotFound.less'
 
 class NotFound extends Component<any, any> {
 
   render() {
     return (
-      <p>NotFound</p>
+      <div className='notMatch'>
+        <p className='text'>404</p>
+        <p>你要找的页面不存在</p>
+        <a href="/">返回首页</a>
+      </div>
     )
   }
 }
