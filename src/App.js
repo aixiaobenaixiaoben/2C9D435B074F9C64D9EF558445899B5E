bae8192cf7158bc2d41e04455623b663a57da208
kevin <357620917@qq.com>
2018-10-19 16:50:36 +0800

    localize with antd

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import {Provider} from 'react-redux'
 import {PersistGate} from "redux-persist/integration/react"
+import {LocaleProvider} from 'antd'
+import zh_CN from 'antd/lib/locale-provider/zh_CN'
 
 import {app} from './modules'
 import createStore from './createStore'
@@ -11,13 +13,15 @@ const {store, persistor} = createStore()
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Router>
-          <Route component={(props) => <app.AppMain {...props}/>}/>
-        </Router>
-      </PersistGate>
-    </Provider>
+    <LocaleProvider locale={zh_CN}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Router>
+            <Route component={(props) => <app.AppMain {...props}/>}/>
+          </Router>
+        </PersistGate>
+      </Provider>
+    </LocaleProvider>
   )
 }
 
